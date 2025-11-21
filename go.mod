7dde52d4a1bcc61f73cb7dcb613cd289cf746041
Gabriel <dealmeida.gabriel.d@gmail.com>
2025-11-21 17:41:02 -0300

    adding migratto and templating the html files

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.org/gabrielga-dev/raw-go-products-api
 
 go 1.22.2
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/gabrielga-dev/migratto v1.1.2 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	dto "github.com/gabrielga-dev/migratto/dto"
+	migration_service "github.com/gabrielga-dev/migratto/service/migration"
 	"github.org/gabrielga-dev/raw-go-products-api/routes"
 )
 
 func main() {
+	err := inicializaBancoDeDados()
+	if err != nil {
+		fmt.Println("Error during database initialization:", err)
+		return
+	}
 	routes.CarregaRotas()
 	http.ListenAndServe(":8000", nil)
+}
+
+func inicializaBancoDeDados() error {
+	config := dto.ConfigDTO{
+		DatabaseDriver:   "postgres",
+		DatabaseHost:     "localhost",
+		DatabasePort:     5432,
+		DatabaseName:     "go-produtos",
+		DatabaseUsername: "admin",
+		DatabasePassword: "admin123",
+		Sslmode:          "disable",
+		MigrationsDir:    "./migrations",
+		Log:              true,
+	}
+
+	fmt.Println("Starting migrations with Migratto...")
+	return migration_service.Migrate(config)
 }
